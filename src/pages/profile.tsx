1fa4ddcbf03da5c04c9418f9fce3f2beaf0fea47
SiddharthManjul <manjulsiddharth09@gmail.com>
2023-12-05 23:35:51 +0530

    Basic Frontend Completed

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,84 @@
+import { useState } from "react";
+import Switch from "@mui/material/Switch";
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
 const Profile = () => {
-    return (
-        <>
-            <p>profile</p>
-        </>
-    )
-}
+  const [formData, setFormData] = useState({
+    file: "",
+    title: "",
+    description: "",
+    tags: "",
+  });
+
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    alert(
+      `File: ${formData.file}, Title: ${formData.title}, description: ${formData.description}, tags: ${formData.tags}`
+    );
+  };
+  return (
+    <>
+      <div className="container m-auto grid place-items-center font-sans mt-8">
+        <label htmlFor="name">Choose file to Upload:</label>
+        <input
+          className="mb-4 h-12 pl-28 mt-4"
+          type="file"
+          id="file"
+          name="file"
+          value={formData.file}
+          onChange={handleChange}
+        />
+        <p><span>Sponsored?  </span><Switch {...label} /></p>
+        <form className="flex flex-col w-96" onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2"
+            placeholder="Video Title..."
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="email">Email:</label>
+          <textarea
+            className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2 pt-2"
+            placeholder="Video Description..."
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="message">Tagst:</label>
+          <input
+            className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2"
+            placeholder="Tags..."
+            type="tags"
+            id="tags"
+            name="tags"
+            value={formData.tags}
+            onChange={handleChange}
+          />
+          <div className="grid place-items-center">
+            <button
+              className="border-2 border-solid border-sky-600 bg-sky-200 rounded-lg p-2 uppercase mb-4"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
--- src/pages/sponsor.tsx
+++ src/pages/sponsor.tsx
@@ -1,11 +1,88 @@
+import { useState } from "react";
+
 const Sponsor = () => {
-    return (
-        <>
-            <div>
-                <p>To become a Sponsor, provide all the details below</p>
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    number: "",
+  });
+
+  const handleChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    alert(
+      `Name: ${formData.name}, Email: ${formData.email}, Number: ${formData.number}, Message: ${formData.message}`
+    );
+  };
+
+  return (
+    <>
+      <div className="container m-auto mt-8 grid place-items-center font-sans text-xl">
+        <div className="mb-4">
+          <p>To become a Sponsor, provide all the details below</p>
+        </div>
+        <div className="grid place-items-center">
+          <form className="flex flex-col w-96" onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input
+              className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2"
+              placeholder="Name..."
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input
+              className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2"
+              placeholder="Email..."
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="message">Sponsoring Amount:</label>
+            <input
+              className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2"
+              placeholder="Sponsoring Amount..."
+              id="number"
+              name="number"
+              value={formData.number}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="message">Media:</label>
+            <input
+              className="border-2 border-solid border-black rounded-lg mb-4 h-12 pl-2"
+              placeholder="Link to Media..."
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            />
+
+            <div className="grid place-items-center">
+              <button
+                className="border-2 border-solid border-sky-600 bg-sky-200 rounded-lg p-2 uppercase mb-4"
+                type="submit"
+              >
+                Submit
+              </button>
             </div>
-        </>
-    )
-}
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Sponsor;
